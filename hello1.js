a8bf7c7e897f3a47ea168429a4ea277e6688bb50
Talha12Shiekh <tk.shiekh4567@gmail.com>
2025-09-13 04:24:19 -0700

    Fixed the code breaking error in MAIN

--- hello1.js
+++ hello1.js
@@ -2,3 +2,5 @@ console.log("Hello git !");
 console.log("Hello again git");
 console.log("Hello again and again git");
 console.log("Hello again and again and again git");
+
+console.log("Fixed the error");
