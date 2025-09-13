b6ba0814a289f283bf79e45fc5860bdf8b31d378
Talha12Shiekh <tk.shiekh4567@gmail.com>
2025-09-13 04:09:36 -0700

    Revert "BREAKED THE CODE BY ADDING THE LOG"
    This reverts commit 7e365523297b9b16db6024b7fa6365b01275afbb.

--- hello1.js
+++ hello1.js
@@ -1,4 +1,3 @@
 console.log("Hello git !");
-console.log("Hello again git");
-console.log("Hello again and again git");
-console.log("Hello again and again and again git");
+console.log("Hello Github !");
+console.log("TRYING THE CODE!");
