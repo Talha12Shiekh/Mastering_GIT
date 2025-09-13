b8c9af80523b9c783310e86d65c2d729a98d92e1
Talha12Shiekh <tk.shiekh4567@gmail.com>
2025-09-13 04:11:58 -0700

    Revert "Added some more git console.logs"
    This reverts commit 0ce85b8ef1443cff1408e1097b8b77ba80754a55.

--- hello1.js
+++ hello1.js
@@ -1,3 +1,4 @@
 console.log("Hello git !");
-console.log("Hello Github !");
-console.log("TRYING THE CODE!");
+console.log("Hello again git");
+console.log("Hello again and again git");
+console.log("Hello again and again and again git");
